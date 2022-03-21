af676dffcf01c6de754a1bff14f8d471f6763123
denisero <79418860+denisero21@users.noreply.github.com>
2022-03-21 18:12:53 +0300

    chages

--- lab1/solution.py
+++ lab1/solution.py
@@ -2,24 +2,28 @@ import re
 import os
 import sys
 
-path = 'text.txt'
+
+PATH = 'text.txt'
 
 
 def is_empty() -> bool:
-    return os.path.getsize(path)
+    return os.path.getsize(PATH)
+
 
 def read_file(path: str) -> str:
     if not is_empty():
         raise EOFError("File is empty.")
     else:
-        with open(path) as open_file:
+        with open(PATH) as open_file:
             s = open_file.read()
     return s
+
 
 def check_variables(N: int, K: int):
     if N == 0 or K == 0:
         raise ValueError("Incorrect input")
 
+        
 def frequency_list(s: str) -> list:
     cnt = {}
     for i in re.sub('[^\w\s]', ' ', s.lower().replace('\n', ' ')).split(' '):
@@ -27,6 +31,7 @@ def frequency_list(s: str) -> list:
             cnt.setdefault(i, 0)
             cnt[i] += 1
     return sorted(cnt.items(), key=lambda item: item[1])
+
 
 def words_counts(s: str) -> list:
     cnt = []
@@ -39,12 +44,15 @@ def words_counts(s: str) -> list:
             cnt += [words]
     return cnt
 
+
 def average(cnt: list) -> float:
     return sum(cnt) / len(cnt)
+
 
 def median(cnt: list) -> float:
     cnt = sorted(cnt)
     return (cnt[len(cnt) // 2] + cnt[(len(cnt) - 1) // 2]) / 2
+
 
 def n_gramms(s: str, K: int) -> str:
     cnt = {}
