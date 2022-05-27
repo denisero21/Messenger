d1144c0f484f24f6d4d3b0fc3e30fef45c069c58
denisero <79418860+denisero21@users.noreply.github.com>
2022-05-28 00:31:48 +0300

    deploy commit 4

--- messenger/messenger/settings.py
+++ messenger/messenger/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'mesden.herokuapp.com']
 
@@ -156,6 +156,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static", "static_dirs"),
 )
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
